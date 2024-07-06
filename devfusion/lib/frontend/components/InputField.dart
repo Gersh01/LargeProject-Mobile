8b6b294e045dec72f2a63066d604f09942114f20
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-07-06 17:59:53 -0400

    Create InputField.dart, to be used as component for anywhere there is text input

---
+++ devfusion/lib/frontend/components/InputField.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+
+  final String? placeholderText;
+  const InputField({
+    super.key, this.placeholderText
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Padding(
+
+      padding: EdgeInsets.all(30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+        children: [
+
+          DecoratedBox(decoration: BoxDecoration(
+              color: Color.fromRGBO(124, 58, 237, 1)
+              )),
+
+          Text(
+            placeholderText!,
+            style: const TextStyle(
+              color: Colors.white,
+
+            )),
+
+          Center(
+            child: TextField(
+              style: const TextStyle(
+                color: Colors.white
+              ),
+
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: placeholderText,
+                hintStyle: const TextStyle(
+                  color: Colors.white
+                )
+              )
+            ),
+          ),
+        ]
+      ),
+    ));
+  }
+}
