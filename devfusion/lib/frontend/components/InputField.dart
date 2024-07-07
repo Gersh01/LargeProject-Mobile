7edb56f4d0064148230bffa7e1b857f681b2452e
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-07-06 20:28:28 -0400

    Formatting Fixes

--- devfusion/lib/frontend/components/InputField.dart
+++ devfusion/lib/frontend/components/InputField.dart
@@ -9,42 +9,63 @@ class InputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Padding(
+    return Center(child: Container(
+      height: 64,
+      margin: const EdgeInsets.only(bottom:10),
+      //Box Decoration
+      decoration: const BoxDecoration(
 
-      padding: const EdgeInsets.all(30.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-
-        children: [
-
-          const DecoratedBox(decoration: BoxDecoration(
-              color: Color.fromRGBO(124, 58, 237, 1)
-              )),
-
-          Text(
-            placeholderText!,
-            style: const TextStyle(
-              color: Colors.white,
-
-            )),
-
-          Center(
-            child: TextField(
-              style: const TextStyle(
-                color: Colors.white
-              ),
-
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: placeholderText,
-                hintStyle: const TextStyle(
-                  color: Colors.white
-                )
-              )
-            ),
-          ),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        color: Color.fromRGBO(17, 24, 39, 1),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 2.0),
+            blurRadius: 2.0,
+            color: Color.fromRGBO(0, 0, 0, 0.25),
+          )
         ]
+      ),
+
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0),
+              child: Text(
+                placeholderText!,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'League Spartan',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                )),
+            ),
+
+            SizedBox(
+              height: 18,
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.white
+                ),
+
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: placeholderText,
+                  hintStyle: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'League Spartan',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  )
+                )
+              ),
+            ),
+          ]
+        ),
       ),
     ));
   }
