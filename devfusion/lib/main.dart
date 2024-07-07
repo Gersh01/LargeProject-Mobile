afd533ecd7fb470ab13f6887876d2211c266ef48
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-07-06 20:34:06 -0400

    Fixed Formatting Issues

--- devfusion/lib/main.dart
+++ devfusion/lib/main.dart
@@ -1,150 +1,118 @@
 import 'package:flutter/material.dart';
-
-import 'frontend/pages/ResetPassword.dart';
+import 'frontend/components/InputField.dart';
+import 'frontend/components/Button.dart';
+import 'frontend/components/Divider.dart';
 
 void main() {
   runApp( const MyApp() );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
+    return MaterialApp(
 
-    bool obsdf = true;
+      //Background
+      home: Scaffold(
+        body: Center(
 
-    bool resetPassword = obsdf;
+          //Login Panel
+          child: Container(
+            height:520,
+            width:370,
+            padding: const EdgeInsets.all(30),
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(31, 41, 55, 1),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
 
-    if (resetPassword) {
+            child: const Column(
 
-      return const ResetPassword();
-      
-    } else {
+              children: [
 
-        return MaterialApp(
+              //DevFusion Text
+                Padding(
+                  padding: EdgeInsets.only(bottom: 50.0),
+                  child: Column(
+                    children: [
+                      Center(child: Text(
+                        'Dev Fusion',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'League Spartan',
+                          color: Color.fromRGBO(124, 58, 237, 1),
+                          shadows: [ Shadow(
+                            offset: Offset(0, 4.0),
+                            blurRadius: 20.0,
+                            color: Color.fromRGBO(0, 0, 0, 0.4),
+                          ),]
+                        ),
+                      )),
 
-          //Background
-          home: Scaffold(
-            body: Center(
-
-              //Login Panel
-              child: Container(
-                height:520,
-                width:370,
-                decoration: const BoxDecoration(
-                  color: Color.fromRGBO(31, 41, 55, 1),
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                      Center(child: Text(
+                        'login',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'League Spartan',
+                          color:  Colors.white,
+                          shadows: [  Shadow(
+                            offset: Offset(0, 4.0),
+                            blurRadius: 20.0,
+                            color: Color.fromRGBO(0, 0, 0, 0.4),
+                          )]
+                        ),
+                      )),
+                    ],
+                  ),
                 ),
 
-                child: const Column(
 
-                  children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 50.0),
+                  child: Column(
+                    children: [
+                      InputField(placeholderText: 'Username'),
+                      InputField(placeholderText: 'Password'),
 
-                  //DevFusion Text
-                    Center(child: Text(
-                      'Dev Fusion',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'League Spartan',
-                        color: Color.fromRGBO(124, 58, 237, 1),
-                        shadows: [ Shadow(
-                          offset: Offset(0, 4.0),
-                          blurRadius: 20.0,
-                          color: Color.fromRGBO(0, 0, 0, 0.4),
-                        ),]
-                    ),
-                  )),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'Remember Me', style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 12, fontWeight: FontWeight.w500)
+                            ),
+                          ),
 
-                    Center(child: Text(
-                      'login',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'League Spartan',
-                        color:  Colors.white,
-                        shadows: [  Shadow(
-                          offset: Offset(0, 4.0),
-                          blurRadius: 20.0,
-                          color: Color.fromRGBO(0, 0, 0, 0.4),
-                        )]
+                          Text(
+                              'Forgot Password', style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 12, fontWeight: FontWeight.w500)
+                          )
+                        ],
                       ),
-                    )),
+                    ],
+                  ),
+                ),
 
-                    InputField(placeholderText: 'Username'),
-                    InputField(placeholderText: 'Password'),
-
-                    Row(
-                      children: [
-                        Text(
-                          'Remember Me'
-                        ),
-
-                        Text(
-                          'Forgot Password'
-                        )
-                      ],
-                    )
-                  ]
-                )
-              ),
-            ),
-
-          backgroundColor: const Color.fromRGBO(124, 58, 237, 1)
+                Button(placeholderText: 'Login'),
+                DividerLine(),
+                Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text(
+                      'Sign Up Instead', style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 12, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ]
+            )
           ),
-        );
-      }
+        ),
 
-    }
-}
-
-class InputField extends StatelessWidget {
-
-  final String? placeholderText;
-  const InputField({
-    super.key, this.placeholderText
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Padding(
-      padding: const EdgeInsets.all(30.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: [
-
-          Text(
-            placeholderText!,
-            style: const TextStyle(
-              color: Colors.white,
-
-            )),
-
-          Center(
-            child: TextField(
-              style: const TextStyle(
-                color: Colors.white
-              ),
-
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: placeholderText,
-                hintStyle: const TextStyle(
-                  color: Colors.white
-                )
-              )
-            ),
-          ),
-        ]
+      backgroundColor: const Color.fromRGBO(124, 58, 237, 1)
       ),
-    ));
+    );
   }
 }
+
+
 
