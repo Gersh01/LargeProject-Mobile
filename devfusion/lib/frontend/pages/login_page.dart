f3b1c9607610a445cf7415b84e6224f88dcfd34d
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-07-08 12:41:41 -0400

    connect login api to login button

--- devfusion/lib/frontend/pages/login_page.dart
+++ devfusion/lib/frontend/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
@@ -5,7 +6,8 @@ import '../components/Button.dart';
 import '../components/Divider.dart';
 import '../components/InputField.dart';
 import '../components/DevFusionColoredText.dart';
-
+import 'package:http/http.dart' as http;
+import '../utils/utility.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -15,97 +17,109 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void login() {
+  void login() async {
     print('Username: ${_usernameController.text}');
     print('Password: ${_passwordController.text}');
+    var reqBody = {
+      "login": _usernameController.text,
+      "password": _passwordController.text
+    };
 
-
-
+    var response = await http.post(
+      Uri.parse(loginUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(reqBody),
+    );
+    print(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       //Background
       home: Scaffold(
-        body: Center(
-
+          body: Center(
             //Login Panel
-          child: Container(
-            height:520,
-            width:370,
-            padding: const EdgeInsets.all(30),
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(31, 41, 55, 1),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-            ),
-
-              child: Column(
-
-                children: [
-
+            child: Container(
+                height: 520,
+                width: 370,
+                padding: const EdgeInsets.all(30),
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(31, 41, 55, 1),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Column(children: [
                   //DevFusion Text
                   const Padding(
                     padding: EdgeInsets.only(bottom: 50.0),
                     child: Column(
                       children: [
                         DevFusionColoredText(),
-
-                        Center(child: Text(
+                        Center(
+                            child: Text(
                           'login',
                           style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'League Spartan',
-                            color:  Colors.white,
-                            shadows: [  Shadow(
-                              offset: Offset(0, 4.0),
-                              blurRadius: 20.0,
-                              color: Color.fromRGBO(0, 0, 0, 0.4),
-                            )]
-                          ),
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'League Spartan',
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(0, 4.0),
+                                  blurRadius: 20.0,
+                                  color: Color.fromRGBO(0, 0, 0, 0.4),
+                                )
+                              ]),
                         )),
                       ],
                     ),
                   ),
 
-
                   Padding(
                     padding: const EdgeInsets.only(bottom: 50.0),
                     child: Column(
                       children: [
-                        InputField(placeholderText: 'Username', controller: _usernameController),
-                        InputField(placeholderText: 'Password', controller: _passwordController),
-
+                        InputField(
+                            placeholderText: 'Username',
+                            controller: _usernameController),
+                        InputField(
+                            placeholderText: 'Password',
+                            controller: _passwordController),
                         const Row(
                           children: [
                             Expanded(
-                              child: Text(
-                                  'Remember Me', style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 12, fontWeight: FontWeight.w500)
-                              ),
+                              child: Text('Remember Me',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Poppins',
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500)),
                             ),
-
-                            Text(
-                                'Forgot Password', style: TextStyle(color: Colors.white, fontFamily: 'Poppins', fontSize: 12, fontWeight: FontWeight.w500)
-                            )
+                            Text('Forgot Password',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Poppins',
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500))
                           ],
                         ),
                       ],
                     ),
-                    ),
+                  ),
 
-                  Button(placeholderText: 'Login', backgroundColor: const Color.fromRGBO(124, 58, 237, 1), textColor: Colors.white, onPressed: login),
+                  Button(
+                      placeholderText: 'Login',
+                      backgroundColor: const Color.fromRGBO(124, 58, 237, 1),
+                      textColor: Colors.white,
+                      onPressed: login),
 
                   const DividerLine(),
                   Padding(
                     padding: const EdgeInsets.all(5.0),
-                      child: InkWell(
+                    child: InkWell(
                         child: const Text(
                           'Sign Up Instead',
                           style: TextStyle(
@@ -114,18 +128,13 @@ class _LoginPageState extends State<LoginPage> {
                             fontWeight: FontWeight.w500,
                           ),
                         ),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/signup');
-                      }
-                    ),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/signup');
+                        }),
                   )
-                ]
-              )
-            ),
+                ])),
           ),
-
-        backgroundColor: const Color.fromRGBO(124, 58, 237, 1)
-      ),
+          backgroundColor: const Color.fromRGBO(124, 58, 237, 1)),
     );
   }
 }
---
+++ devfusion/lib/frontend/utils/utility.dart
@@ -0,0 +1,2 @@
+const baseUrl = 'http://localhost:4000/api/mobile/';
+const loginUrl = '${baseUrl}login';
