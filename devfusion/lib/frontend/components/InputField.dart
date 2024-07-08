74219319f8bc7cfedb7009aa947e8a7c98c82b28
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-07-07 22:47:42 -0400

    Update InputField.dart

--- devfusion/lib/frontend/components/InputField.dart
+++ devfusion/lib/frontend/components/InputField.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class InputField extends StatelessWidget {
 
   final String? placeholderText;
@@ -17,57 +18,57 @@ class InputField extends StatelessWidget {
       //Box Decoration
       decoration: const BoxDecoration(
 
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-        color: Color.fromRGBO(17, 24, 39, 1),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 2.0),
-            blurRadius: 2.0,
-            color: Color.fromRGBO(0, 0, 0, 0.25),
-          )
-        ]
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: Color.fromRGBO(17, 24, 39, 1),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 2.0),
+              blurRadius: 2.0,
+              color: Color.fromRGBO(0, 0, 0, 0.25),
+            )
+          ]
       ),
 
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
 
-          children: [
+            children: [
 
-            Padding(
-              padding: const EdgeInsets.only(bottom: 5.0),
-              child: Text(
-                placeholderText!,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'League Spartan',
-                  fontSize: 12,
-                  fontWeight: FontWeight.w600,
-                )),
-            ),
-
-            SizedBox(
-              height: 18,
-              child: TextField(
-                controller: controller,
-                style: const TextStyle(
-                  color: Colors.white
-                ),
-
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: placeholderText,
-                  hintStyle: const TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'League Spartan',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                  )
-                )
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: Text(
+                    placeholderText!,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                    )),
               ),
-            ),
-          ]
+
+              SizedBox(
+                height: 18,
+                child: TextField(
+                    controller: controller,
+                    style: const TextStyle(
+                        color: Colors.white
+                    ),
+
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: placeholderText,
+                        hintStyle: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        )
+                    )
+                ),
+              ),
+            ]
         ),
       ),
     ));
