9737f1790368dbdf6f899c3e16f99f20d33bd1f7
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-07-08 14:18:21 -0400

    Update utility.dart

--- devfusion/lib/frontend/utils/utility.dart
+++ devfusion/lib/frontend/utils/utility.dart
@@ -1,3 +1,26 @@
 const baseUrl = 'http://localhost:4000/api/mobile/';
+//URLs for Users collection APIs
 const loginUrl = '${baseUrl}login';
 const registerUrl = '${baseUrl}register';
+const resendEmailUrl = '${baseUrl}resend_verification_email';
+const updateUserUrl = '${baseUrl}users';
+const resetPasswordUrl = '${baseUrl}users/password';
+const getUserUrl = '${baseUrl}users/';
+const forgotPasswordUrl = '${baseUrl}forgot_password';
+const jwtUrl = '${baseUrl}jwtTest';
+//URLs for Projects collection APIs
+const getProjectUrl = '${baseUrl}project/';
+const createProjectUrl = '${baseUrl}project';
+const editTeamMembersUrl = '${baseUrl}project/team_members';
+const discoverUrl = '${baseUrl}discover';
+const ownedProjectsUrl = '${baseUrl}owned-projects';
+const joinedProjectsUrl = '${baseUrl}joined-projects';
+const ownedJoinedUrl = '${baseUrl}owned-joined';
+const editProjectUrl = '${baseUrl}edit-project';
+const deleteProjectUrl = '${baseUrl}project/';
+const leaveProjectUrl = '${baseUrl}project/leave';
+//URLs for Inbox collection APIs
+const applyInboxUrl = '${baseUrl}inbox/apply';
+const acceptInboxUrl = '${baseUrl}inbox/accept_member';
+const rejectInboxUrl = '${baseUrl}inbox/reject_member';
+const getInboxUrl = '${baseUrl}inbox/';
