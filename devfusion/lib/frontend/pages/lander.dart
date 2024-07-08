6c9697319ad90eecf716a87477367f6dccdd6c47
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-07-07 22:48:47 -0400

    Update lander.dart

--- devfusion/lib/frontend/pages/lander.dart
+++ devfusion/lib/frontend/pages/lander.dart
@@ -6,93 +6,96 @@ class Lander extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+
     return MaterialApp(
         home: Scaffold(
-        backgroundColor: const Color.fromRGBO(124, 58, 237, 1),
+            backgroundColor: const Color.fromRGBO(124, 58, 237, 1),
 
 
 
-        body: Padding(
-          padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 250.0, bottom: 5.0),
-        
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+            body: Padding(
+                padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 150.0, bottom: 5.0),
 
-                const Center(
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 32,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    const Center(
+                        child: Text(
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              fontSize: 32,
+                            ),
+                            "THRIVE IN THE POWER OF OPEN-SOURCE TO DRIVE INNOVATION!"
+                        )
                     ),
-                    "THRIVE IN THE POWER OF OPEN-SOURCE TO DRIVE INNOVATION!"
-                  )
-                ),
 
-                const SizedBox(height: 15),
+                    const SizedBox(height: 15),
 
-                const Center(
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w300,
-                      fontSize: 15,
+                    const Center(
+                        child: Text(
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300,
+                              fontSize: 15,
+                            ),
+                            "Whether you're a seasoned developer or just starting out, you'll find a place here"
+                        )
                     ),
-                    "Whether you're a seasoned developer or just starting out, you'll find a place here"
-                  )
-                ),
 
-                const SizedBox(height: 15),
+                    const SizedBox(height: 15),
 
 
-                const Center(
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w300,
-                      fontSize: 15,
+                    const Center(
+                        child: Text(
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300,
+                              fontSize: 15,
+                            ),
+                            "connect with like-minded individuals who share your passion for coding"
+                        )
                     ),
-                    "connect with like-minded individuals who share your passion for coding"
-                  )
-                ),
 
-                const SizedBox(height: 50),
+                    const SizedBox(height: 50),
 
 
-                const Center(
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20,
+                    const Center(
+                        child: Text(
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20,
+                            ),
+                            "Together, we can build a brighter future, one line of code at a time!"
+                        )
                     ),
-                    "Together, we can build a brighter future, one line of code at a time!"
-                  )
-                ),
 
-                Expanded(
-                  child: Container(
-                    color: const Color.fromRGBO(124, 58, 237, 1),
-                  )
-                ),
+                    Expanded(
+                        child: Container(
+                          color: const Color.fromRGBO(124, 58, 237, 1),
+                        )
+                    ),
 
-                Button(placeholderText: "Get Started", backgroundColor: Colors.white, textColor: Colors.black, 
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/login');
-                  }
-                )
-                
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 30.0),
+                      child: Button(placeholderText: "Get Started", backgroundColor: Colors.white, textColor: Colors.black,
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/login');
+                          }
+                      ),
+                    )
 
-              ],)
+
+                  ],)
             )
-          )
-        );
+        )
+    );
   }
 }
