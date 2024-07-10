ebdc829e9ab9750df9cfa30a4b1105469705bb17
GoldenLin9 <goldenlin0909@gmail.com>
2024-07-09 21:26:53 -0400

    top of discover setup

--- devfusion/lib/frontend/pages/discover.dart
+++ devfusion/lib/frontend/pages/discover.dart
@@ -8,11 +8,9 @@ class Discover extends StatefulWidget {
 }
 
 class _DiscoverState extends State<Discover> {
-
   String _dropdownValue = 'recent';
 
-  void dropdownCallback(String? newValue){
-
+  void dropdownCallback(String? newValue) {
     if (newValue != null) {
       setState(() {
         _dropdownValue = newValue;
@@ -22,8 +20,7 @@ class _DiscoverState extends State<Discover> {
 
   String _dropdownSearchByValue = 'Title';
 
-  void dropdownSearchByCallback(String? newValue){
-
+  void dropdownSearchByCallback(String? newValue) {
     if (newValue != null) {
       setState(() {
         _dropdownSearchByValue = newValue;
@@ -34,77 +31,82 @@ class _DiscoverState extends State<Discover> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Discover',
-
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'League Spartan',
-            color: Colors.white,
-            shadows: [ Shadow(
-              offset: Offset(0, 4.0),
-              blurRadius: 20.0,
-              color: Color.fromRGBO(0, 0, 0, 0.4),
-            ),]
+        appBar: AppBar(
+          title: const Text(
+            'Discover',
+            style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'League Spartan',
+                color: Colors.white,
+                shadows: [
+                  Shadow(
+                    offset: Offset(0, 4.0),
+                    blurRadius: 20.0,
+                    color: Color.fromRGBO(0, 0, 0, 0.4),
+                  ),
+                ]),
           ),
+          backgroundColor: const Color.fromRGBO(31, 41, 55, 1),
         ),
-
-        backgroundColor: const Color.fromRGBO(31, 41, 55, 1),
-
-      ),
-
-      body:       
-      Column(
-
-        children: [
-
-          DropdownButton(
-            items: const [
-              DropdownMenuItem(
-                value: 'Title',
-                child: Text('Title'),
+        body: Column(
+          children: [
+            Row(children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                
+                  child: DropdownButton(
+                    items: const [
+                      DropdownMenuItem(
+                        value: 'Title',
+                        child: Text('Title'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Technology',
+                        child: Text('Technology'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Description',
+                        child: Text('Description'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Role',
+                        child: Text('Role'),
+                      )
+                    ],
+                    value: _dropdownSearchByValue,
+                    onChanged: dropdownSearchByCallback,
+                  ),
               ),
-              DropdownMenuItem(
-                value: 'Technology',
-                child: Text('Technology'),
               ),
-              DropdownMenuItem(
-                value: 'Description',
-                child: Text('Description'),
-              ),
-              DropdownMenuItem(
-                value: 'Role',
-                child: Text('Role'),
-              )
-            ],
-            value: _dropdownSearchByValue,
-            onChanged: dropdownSearchByCallback,
-          ),
 
-          //Search Bar
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                hintStyle: TextStyle(
-                  color: Color.fromRGBO(0, 0, 0, 0.4),
+              //Search Bar
+              const Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      hintStyle: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 0.4),
+                      ),
+                      prefixIcon: Icon(Icons.search),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
                 ),
-                prefixIcon: Icon(Icons.search),
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  borderSide: BorderSide.none,
-                ),
               ),
-            ),
-          ),
+            ]),
 
-          DropdownButton(
-            items: const [
+            //Search By Dropdown
+
+            DropdownButton(items: const [
               DropdownMenuItem(
                 value: 'recent',
                 child: Text('Most Recent'),
@@ -113,29 +115,24 @@ class _DiscoverState extends State<Discover> {
                 value: 'relevance',
                 child: Text('Relevance'),
               )
-            ],
-            value: _dropdownValue,
-            onChanged: dropdownCallback
-          ),
+            ], value: _dropdownValue, onChanged: dropdownCallback),
 
-          //Project Cards
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                    title: Text('Project $index'),
-                    subtitle: const Text('Project Description'),
-                    trailing: const Icon(Icons.more_vert),
-                  ),
-                );
-              },
+            //Project Cards
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text('Project $index'),
+                      subtitle: const Text('Project Description'),
+                      trailing: const Icon(Icons.more_vert),
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
-      
-      )
-    );
+          ],
+        ));
   }
 }
