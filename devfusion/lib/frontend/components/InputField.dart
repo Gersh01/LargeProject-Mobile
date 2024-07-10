0b1e52b8220f2c4cb233dcdf05242b206902759d
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-07-09 20:40:52 -0400

    make the input validation errors invisible

--- devfusion/lib/frontend/components/InputField.dart
+++ devfusion/lib/frontend/components/InputField.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 
-
 class InputField extends StatelessWidget {
-
   final String? placeholderText;
   final TextEditingController? controller;
   final String? Function(String?)? validator;
 
-  const InputField({
-    super.key, this.placeholderText, this.controller, this.validator
-  });
+  const InputField(
+      {super.key, this.placeholderText, this.controller, this.validator});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(
+    return Center(
+        child: Container(
       height: 64,
-      margin: const EdgeInsets.only(bottom:10),
+      margin: const EdgeInsets.only(bottom: 10),
       //Box Decoration
       decoration: const BoxDecoration(
-
           borderRadius: BorderRadius.all(Radius.circular(5)),
           color: Color.fromRGBO(17, 24, 39, 1),
           boxShadow: [
@@ -27,51 +24,39 @@ class InputField extends StatelessWidget {
               blurRadius: 2.0,
               color: Color.fromRGBO(0, 0, 0, 0.25),
             )
-          ]
-      ),
+          ]),
 
       child: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-
-            children: [
-
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5.0),
-                child: Text(
-                    placeholderText!,
-                    style: const TextStyle(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 5.0),
+            child: Text(placeholderText!,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Poppins',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                )),
+          ),
+          SizedBox(
+            height: 18,
+            child: TextFormField(
+                validator: validator,
+                controller: controller,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: placeholderText,
+                    errorStyle: const TextStyle(fontSize: 0),
+                    hintStyle: const TextStyle(
                       color: Colors.white,
                       fontFamily: 'Poppins',
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
-                    )),
-              ),
-
-              SizedBox(
-                height: 18,
-                child: TextFormField(
-                    validator: validator,
-                    controller: controller,
-                    style: const TextStyle(
-                        color: Colors.white
-                    ),
-
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: placeholderText,
-                        hintStyle: const TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        )
-                    )
-                ),
-              ),
-            ]
-        ),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ))),
+          ),
+        ]),
       ),
     ));
   }
